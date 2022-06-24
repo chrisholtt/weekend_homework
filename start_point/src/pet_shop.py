8cad6a4e6cd2bf8b4bc09ea0a78c4b59b971fdb4
Chris <chrisphilholt@gmail.com>
2022-06-24 15:13:21 +0100

    another update

--- start_point/src/pet_shop.py
+++ start_point/src/pet_shop.py
@@ -20,3 +20,51 @@ def get_pets_sold(dict):
 def increase_pets_sold(dict, number_of_pets):
     dict["admin"]["pets_sold"] += number_of_pets
     return dict
+
+
+def get_stock_count(dict):
+    stock_number = len(dict["pets"])
+    return stock_number
+
+
+def get_pets_by_breed(dict, breed):
+    pets_with_name = []
+    for item in dict["pets"]:
+        if item["breed"] == breed:
+            pets_with_name.append(item)
+    return pets_with_name
+
+
+def find_pet_by_name(dict, name):
+    for item in dict["pets"]:
+        if item["name"] == name:
+            return item
+
+
+def remove_pet_by_name(dict, name):
+    index = -1
+    for item in dict["pets"]:
+        index += 1
+        if item["name"] == name:
+            dict["pets"].pop(index)
+    return dict
+
+
+def add_pet_to_stock(dict, new_pet):
+    dict["pets"].append(new_pet)
+    return dict
+
+
+def get_customer_cash(customer):
+    return customer["cash"]
+
+
+def remove_customer_cash(customer, amount):
+    customer["cash"] -= amount
+    return customer
+
+
+def get_customer_pet_count(customer):
+    pet_count = 0
+    pet_count += len(customer["pets"])
+    return pet_count
