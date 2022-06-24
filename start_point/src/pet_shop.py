09f3cf38bacdccef967de4c385166f3faf539c21
Chris <chrisphilholt@gmail.com>
2022-06-24 13:00:30 +0100

    slightly updated version

--- start_point/src/pet_shop.py
+++ start_point/src/pet_shop.py
@@ -1,1 +1,18 @@
 # WRITE YOUR FUNCTIONS HERE
+
+
+def get_pet_shop_name(list):
+    return list["name"]
+
+
+def get_total_cash(dict):
+    return dict["admin"]["total_cash"]
+
+
+# def add_or_remove_cash(dict, cash):
+#     prev_ammount = dict["admin"]["total_cash"]
+#     prev_ammount += cash
+#     return prev_ammount
+
+def add_or_remove_cash(dict, cash):
+    dict["admin"]["total_cash"] += cash
